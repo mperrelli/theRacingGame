b85b49a368821e07aa1b0907e7fee3d8396d5345
Matt Perrelli <matp919@gmail.com>
2014-03-02 14:01:28 -0500

    Added gamestate switch and alot of comments for the dev process

--- template.cpp
+++ template.cpp
@@ -1,16 +1,90 @@
+// Programmers: Matt Perrelli, Anthony Mascia, Andrew Goreckie
+// Course:	    CMPT 456 
+// Due Date:    N/A
+//
+// The Racing Game
+//
+// Description will go here
+
 // Includes, namespace and prototypes
 #include "template.h"
 using namespace AGK;
 app App;
 
+// Gamestates
+const int TITLESCREEN = 0;
+const int PICKMAP = 1;
+const int PICKVEHICLE = 2;
+const int INPLAY = 3;
+int g_gameState = PICKMAP;
+
 // Begin app, called once at the start
 void app::Begin( void )
 {
+	// Set the window title.
+    agk::SetWindowTitle("The Racing Game");
 }
 
 // Main loop, called every frame
 void app::Loop ( void )
 {
+
+	/* 
+	* Every iteration of the main loop we need to check for user input
+	* We check the keyboard, cursor position, and mouse clicks.
+	*
+	* The following functions will need to be implemented
+	*
+	* updateCursor();
+	* checkMouseInput();
+	* checkKeyboardInput();
+	*
+	*/
+
+	switch(g_gameState)
+	{
+	case TITLESCREEN:
+
+		/*
+		* Some fancy looking design to make our project look good
+		*/
+
+		break;
+
+	case PICKMAP:
+
+		/*
+		* This is where we build a screen that displays map selection.
+		* Right now this is a choice between two maps. Linear or Cyclical.
+		* user clicks on one of the other and then we generate a map based on that.
+		*
+		* The user should also be selecting a difficulty here
+		*/
+
+		break;
+
+	case PICKVEHICLE:
+
+		/*
+		* The user will select a vehicle. I'm not really sure where we want to go with this.
+		* We should at least be selecting between control, speed or balanced and a color.
+		*/
+
+		break;
+
+	case INPLAY:
+	
+		/*
+		* do somthing.. this will probably include checking
+		* for input and updateing the screen IE updateing the
+		* environment
+		*/
+
+		break;
+	}
+
+	// Update the screen every iteration of the loop
+	agk::Sync();
 }
 
 // Called when the app ends
