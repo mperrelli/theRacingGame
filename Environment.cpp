ec51e224bc4d9d49c96767e5d2e9b806d44e5df0
Matt Perrelli <matp919@gmail.com>
2014-04-03 00:01:29 -0400

    Implemented alot of functionality

--- Environment.cpp
+++ Environment.cpp
@@ -1,17 +1,31 @@
+#include "template.h"
 #include "Environment.h"
+#include <string>
+#include <vector>
+using namespace AGK;
 
 Environment::Environment(int diff, int t)
 {
 	difficulty = diff;
 	type = t;
+	positionX = 0;
+	positionY = 0;
+	tileRows = 0;
+	tileCols = 0;
+	trackAtlas = NULL;
 
 	/*
 	* Once we know the difficulty and track type we can then call the next
 	* two functions to construct what makes up the environment
 	*/
 
+	loadTiles();
+
 	generateTrack(t);
-	addObstacles(diff);
+
+	createSprites();
+
+	draw();
 
 	/* 
 	* Once we have done all these functions we should have a complete
@@ -27,16 +41,34 @@ void Environment::generateTrack(int type)
 {
 	/*
 	* This is where all the logic for building our 
-	* track will occure.
+	* track will occure. 
 	*/
-}
 
-void Environment::addObstacles(int difficulty)
-{
-	/*
-	* After we construct the track we want to place obstacles on and around it
-	* all the logic for placeing obstacles is here.
-	*/
+	switch(type)
+	{
+	case LINEAR:
+
+		int track[LINEAR_ROWS][LINEAR_COLS] =
+		{ {GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS},
+		  {GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS},
+		  {TRACK, TRACK, TRACK, TRACK, TRACK, TRACK, TRACK, TRACK, TRACK, TRACK},
+		  {GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS},
+		  {GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS},
+		};
+
+		trackAtlas = track;
+
+		positionX = 0;
+		positionY = -80;
+
+		tileRows = LINEAR_ROWS;
+		tileCols = LINEAR_COLS;
+
+		break;
+	case CYCLICAL:
+
+		break;
+	}
 }
 
 void Environment::updateEnvironment()
@@ -68,3 +100,61 @@ float Environment::getPositionY()
 	return positionY;
 }
 
+void Environment::draw()
+{
+	// Variables for the tile coordinates
+	float x = 0, y = 0;
+
+	// Variable to temporarily hold a sprite index
+	int spriteIndex = TRACK_ATLAST_START_INDEX;
+
+	// Display all the tiles specified in the map.
+	for (int r = 0; r < tileRows; r++)
+	{
+		// Set x to 0.
+		x = 0;
+
+		// Display all the tiles in this row.
+		for (int c = 0; c < tileCols; c++)
+		{
+			// Set the tile's position.
+			agk::SetSpritePosition(spriteIndex, x, y);
+
+			// Increment sprite Index
+			spriteIndex++;
+
+			// Update the X coordinate for the next tile.
+			x += TRACK_TILE_SIZE;
+		}
+
+		// Increase y for the next row.
+		y += TRACK_TILE_SIZE;
+	}
+}
+
+void Environment::loadTiles()
+{  
+   agk::LoadImage(GRASS,          "Resources/grass.png");
+   agk::LoadImage(TRACK,          "Resources/track.png");
+   agk::LoadImage(TRACK_TURN,     "Resources/trackturn.png");
+   agk::LoadImage(BARRIER,        "Resources/barrier.png");
+   agk::LoadImage(BARRIER_TURN,   "Resources/barrierturn.png");
+}
+
+void Environment::createSprites()
+{
+	int spriteIndex = TRACK_ATLAST_START_INDEX;
+
+	for (int r = 0; r < tileRows; r++)
+	{
+		// Display all the tiles in this row.
+		for (int c = 0; c < tileCols; c++)
+		{
+			// Create a sprite for this tile.
+			agk::CreateSprite(spriteIndex, trackAtlas[r][c]);
+
+			// Increment sprite Index
+			spriteIndex++;
+		}
+	}
+}
--- Environment.h
+++ Environment.h
@@ -3,19 +3,48 @@
 class Environment
 {
 private:
-	int difficulty, type;
-	float positionX, positionY;
+	static const int LINEAR = 1;
+	static const int CYCLICAL = 2;
+
+	static const int TRACK_TILE_SIZE = 128;
+	static const int OBSTACLE_TILE_SIZE = 32;
+
+	static const int LINEAR_ROWS = 5;
+	static const int LINEAR_COLS = 10;
+
+	static const int TRACK_ATLAST_START_INDEX = 50;
+
+	// Constants for the image indecies
+	static const int GRASS         = 10;
+	static const int TRACK         = 11;
+	static const int TRACK_TURN    = 12;
+	static const int BARRIER       = 13;
+	static const int BARRIER_TURN  = 14;
+
+	int difficulty;
+	int type;
+
+	float positionX;
+	float positionY;
+
+	int * trackAtlas;
+
+	int tileRows;
+	int tileCols;
+
+	void loadTiles();
+	void createSprites();
 
 public:
 	Environment(int difficulty, int type);
 	~Environment(void);
 
 	void generateTrack(int type);
-	void addObstacles(int difficulty);
 	void updateEnvironment();
 	void setPositionX(float posX);
 	void setPositionY(float posY);
-	
+	void draw();
+
 	float getPositionX();
 	float getPositionY();
 };
