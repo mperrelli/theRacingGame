dd2c8ecc856ba37d1cf19392c465c70c180d4b2b
Anthony Mascia <amascia.student@manhattan.edu>
2014-04-09 15:27:51 -0400

    Finished up Vehicle class.

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,23 +1,75 @@
 #include "template.h"
 #include <string>
 #include "Vehicle.h"
+#include <iostream>
 
 using namespace std;
+using namespace AGK;
 
 // Constructor
-Vehicle::Vehicle(int sIndex, int maxSpeed, int controlFactor){}
-
-void Vehicle::setColor(string color)
+Vehicle::Vehicle()
 {
-	// SetSpriteColor(sIndex, INT, INT, INT, INT);
+	// These values can change, just temporary place holders
+	health = 100;
+	maxSpeed = 100;
+	controlFactor = 1;
+	active = false;
+}
+
+int Vehicle::getControlFactor()
+{
+	return controlFactor();
 }
 
 int Vehicle::getHealth()
 {
-	return this->health;
+	return health;
 }
 
-void Vehicle::setHealth(int health)
+int Vehicle::getMaxSpeed()
 {
-	this->health = health;
+	return maxSpeed;
+}
+
+void Vehicle::setActive(bool a)
+{
+	active = a;
+}
+
+void Vehicle::setColor(char color)
+{
+	// 'R' for RED
+	// 'G' for GREEN
+	// 'B' FOR BLUE
+	switch(color)
+	{
+		case 'R': 
+			agk::SetSpriteColor(spriteIndex, 255, 0, 0, 255);
+			break;
+		case 'G':
+			agk::SetSpriteColor(spriteIndex, 50, 205, 50, 255);
+			break;
+		default:
+			agk::SetSpriteColor(spriteIndex, 0, 199, 255, 255);
+	}
+}
+
+void Vehicle::setControlFactor(int cf)
+{
+	controlFactor = cf;
+}
+
+void Vehicle::setHealth(int h)
+{
+	health = h;
+}
+
+void Vehicle::setMaxSpeed(int ms)
+{
+	maxSpeed = ms;
+}
+
+bool Vehicle::isActive()
+{
+	return active;
 }
